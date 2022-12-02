3a0947c844a7fe49816c06cca562a045d94814f2
William Hirsch <Williamhirschh@gmail.com>
2022-12-01 18:53:46 -0600

    added models and all objects needed

---
+++ models/Tag.js
@@ -0,0 +1,31 @@
+//Gets connections
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+class Tag extends Model {}
+
+
+
+
+//Makes tag template
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
